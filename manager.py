6a1a439b916274029c4037c63cf1764c5ccc5d3e
voeth <13531457409@163.com>
2019-03-12 12:09:40 +0800

    实现了两个班人数点名的功能，但还不能保存选择

--- manager.py
+++ manager.py
@@ -8,15 +8,9 @@ from flask import Flask,render_template,request,escape
 from random import randint
 app = Flask(__name__)
 
-with open(r'C:\Users\mr。lang\Desktop\callname\static\pm_classmates.csv',encoding='utf8') as pm:
-    index = 0
-    students = {}
-    for n in pm:
-        name = n.split(',')
-        index +=1
-        students[index] = name[2].strip('"\n')
+
 @app.route('/')
-def searh() -> 'html':
+def hello() -> 'html':
     "确定首页的框架、样式"
     title = '欢迎使用点名宝'
     return render_template('home.html',
@@ -24,17 +18,45 @@ def searh() -> 'html':
 
 @app.route('/callname',methods = ['GET','POST']) 
 def return_result()->'html':
-    global students,index
+    #代码放在这个位置，一开始使用get方式来访问/callname，就会立即触发request.form['times']，而此时用户还未有任何操作，所以会抛出keyerror异常
+    #if request.form['times'] == 'am':
+        #path = r'C:\Users\mr。lang\Desktop\callname\static\am.csv'
+    #elif request.form['times'] == 'night':
+       # path = r'C:\Users\mr。lang\Desktop\callname\static\night.csv'
+   # with open(path,encoding='utf8') as file:
+        #index = 0
+        #students = {}
+        #for n in file:
+            #name = n.split(',')
+            #index +=1
+        #students[index] = name[1].strip('"\n')
+
+    title = '抽奖环节'
     if request.method=='GET':
         return render_template("return_name.html",
-                           the_title='抽奖环节',)
+                           the_title=title,)
     if request.method=='POST':
-        global students,index
+    # 对于获取表单请求的数据，应该放在使用post方法访问web，用户有操作产生了数据后再触发request.form['times']，否则会产生keyerror异常
+        if request.form['times'] == 'am':
+            path = r'C:\Users\mr。lang\Desktop\callname\static\am.csv'
+        elif request.form['times'] == 'night':
+            path = r'C:\Users\mr。lang\Desktop\callname\static\night.csv'
+        with open(path,encoding='utf8') as file:
+            index = 0
+            students = {}
+            for n in file:
+                name = n.split(',')
+                index +=1
+                students[index] = name[1].strip('"\n')
         return render_template("return_name.html",
-                   the_title='抽奖环节',
+                   the_title=title,
                    the_students=students[randint(1,index)])
-                          
-    
+               
+
+@app.route('/table',methods = ['GET','POST'])
+def table()->'html':
+    title=('学号','班级')
+    pass
             
 app.run(debug=True)
 
